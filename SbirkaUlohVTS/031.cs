9083bdd53fe4ef9528b482d5e40c26ce5d8a0641
nexu <moatttyas@seznam.cz>
2023-02-28 09:30:50 +0100

    31

---
+++ SbirkaUlohVTS/031.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbirkaUlohVTS
+{
+    /*Napište program, který načítá na vstupu velká písmena a tiskne malá písmena, dokud
+    nebude stisknuto malé nebo velké q
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pište písmena dokud nenapíšete q tak budu ukládat.");
+            string vystup = "";
+            char znak;
+            while ((znak = Console.ReadKey().KeyChar).ToString().ToLower() != "q")
+            {
+                if (znak >= 65 && znak <= 90)
+                {
+                    vystup += znak;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(vystup.ToLower());
+            Console.ReadLine();
+        }
+    }
+}
