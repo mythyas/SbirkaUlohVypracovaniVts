c3e061d698351005cc9763b27d0adb23100db505
nexu <moatttyas@seznam.cz>
2023-03-02 15:32:30 +0100

    skoro sem začal

---
+++ SbirkaUlohVTS/041.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SbirkaUlohVTS
+{
+    /*Napište program, který zkopíruje obsah souboru stary.txt do souboru novy.txt. Navíc 
+    *převede velká písmena na malá, vypustí číslice a místo mezer přejde vždy na nový řádek.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream()
+        }
+    }
+}
